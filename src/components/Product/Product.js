5889814aa78be2a39d0a48d8bbe9472c9e98a209
Noman Sadick <nomansadickjs@gmail.com>
2020-07-11 11:55:18 +0600

    ProductDetails product parameter complit

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,17 +2,18 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css';
+import { Link } from 'react-router-dom';
 
 const Product = (props) => {
-    console.log(props)
-    const { name, img, seller, price, stock } = props.product;
+    console.log(props.product)
+    const { name, img, seller, price, stock, key } = props.product;
     return (
         <div className="product">
             <div>
                 <img src={img} alt="" />
             </div>
             <div>
-                <h3 className="product-name">{name}</h3>
+                <h3 className="product-name"><Link to={"/product/"+key}>{name}</Link></h3>
                 <br />
                 <p>by:{seller}</p>
                 <br />
